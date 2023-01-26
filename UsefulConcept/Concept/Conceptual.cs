f202fa7866c433b78bf3a5377a4131454a9b71cf
alvan <alvan.ghon@gmail.com>
2023-01-26 11:32:01 +0700

    add project C# and make class for trying useful conceptual in C#

---
+++ UsefulConcept/Concept/Conceptual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulConcept.Nullable
+{
+    internal class Conceptual
+    {
+        //variable reference Type
+        string? name = "Hello";
+
+        //variable value Type
+    }
+}
